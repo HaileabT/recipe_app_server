64b355d6afe14e7bbc552ea5e2ca77b055124ee9
Habtemariam <habteshbaki2014@gmail.com>
2025-01-29 12:23:13 -0800

    added database connections

--- database/datasource.ts
+++ database/datasource.ts
@@ -4,14 +4,14 @@ import { ENV } from "../shared/env";
 
 export const appDataSource = new DataSource({
   type: "postgres",
-  host: ENV.dbHost,
-  username: ENV.dbUsername,
-  password: ENV.dbPassword,
-  database: ENV.dbName,
-  port: parseInt(ENV.dbPort ?? "") || 5432,
+  host: "localhost",
+  username: "postgres",
+  password: "habtesh01",
+  database: "Recipe app",
+  port: parseInt(ENV.dbPort ?? "") | 5432,
   entities: [__dirname + "/entities/**/*.ts"],
   logging: true,
   synchronize: true,
 });
 
-console.log(__dirname + "/entities/**/*.ts");
+// console.log(__dirname + "/entities/**/*.ts");
