87250ed882c5baa9c9ae0098bee14ce80041e12a
HaileabT <haileabtesfaye11@gmail.com>
2025-01-29 04:03:25 -0500

    defined typeorm implementations for the domain entities

---
+++ database/entities/UserEntity.ts
@@ -0,0 +1,18 @@
+import { Column, Entity, OneToMany, PrimaryGeneratedColumn } from "typeorm";
+import { IUser } from "../../domain/entities/IUser";
+import { RecipeEntity } from "./RecipeEntity";
+
+@Entity("user")
+export class UserEntity implements IUser {
+  @PrimaryGeneratedColumn("uuid")
+  id!: string;
+
+  @Column()
+  username!: string;
+
+  @Column()
+  password!: string;
+
+  @OneToMany(() => RecipeEntity, (recipe: RecipeEntity) => recipe.user)
+  recipes?: RecipeEntity[];
+}
