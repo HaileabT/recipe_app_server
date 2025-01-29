69c72aa5bcb1668d4fec62fe75baa64b31ce2f9e
Habtemariam <habteshbaki2014@gmail.com>
2025-01-29 12:25:09 -0800

    added cors and cookie parser

--- server.ts
+++ server.ts
@@ -1,11 +1,8 @@
-<<<<<<< HEAD
+import { initDatabase } from "./database/database";
 import { app } from "./api/api";
+
+initDatabase();
 
 app.listen(3000, () => {
   console.log(`server running on port 3000`);
 });
-=======
-import { initDatabase } from "./database/database";
-
-initDatabase();
->>>>>>> 236114768cdcf59935216f48d7501b8c0968e9ae
