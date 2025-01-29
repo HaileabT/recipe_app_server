9493e0f3bf6c910c6a6bbe1be852b737b16854d3
Habtemariam <habteshbaki2014@gmail.com>
2025-01-29 01:10:43 -0800

    added user controller

---
+++ api/controller/user.controller.ts
@@ -0,0 +1,17 @@
+import { Request, Response } from "express";
+
+export const find = (req: Request, res: Response) => {
+  res.send("hello");
+};
+
+export const findOne = (req: Request, res: Response) => {
+  res.send("hello");
+};
+
+export const create = (req: Request, res: Response) => {
+  res.send("hello");
+};
+
+export const deleteOne = (req: Request, res: Response) => {
+  res.send("hello");
+};
